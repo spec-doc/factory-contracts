95b3175a40b5caadc83925a52fa1fc609d5f6748
Ivan Mezinov <ivanmezinov@mail.ru>
2024-08-11 03:24:12 +0300

    add Factory, Token contracts
    * add Factory contract
    * add Token contract

---
+++ Factory/FactoryExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpecDoc\Specificator\Contracts\Factory;
+
+/**
+ * This exception interface describing any factory errors.
+ */
+interface FactoryExceptionInterface extends \Throwable {}
